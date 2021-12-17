c843b151f71f35b0cc570358e5da4cf561c7284a
ardhan <ardhan.s.pratama@gmail.com>
2021-12-17 20:42:29 +0800

    Added fragment switching in MainActivity

--- app/src/main/java/com/example/easypass/MainActivity.java
+++ app/src/main/java/com/example/easypass/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.easypass;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView navBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new HomeFragment()).commit();
+
+        navBar = findViewById(R.id.mainNavBar);
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment frag = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        frag = new HomeFragment();
+                        break;
+
+                    case R.id.nav_search:
+                        frag = new SearchFragment();
+                        break;
+
+                    case R.id.nav_settings:
+                        frag = new SettingsFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, frag).commit();
+                return true;
+            }
+        });
     }
 
     @Override
