f6d3a3d946e423d931598b588d771df90e8f289f
ardhan <ardhan.s.pratama@gmail.com>
2022-01-05 20:58:28 +0800

    Added FAB functionality to create new logins

--- app/src/main/java/com/example/easypass/HomeFragment.java
+++ app/src/main/java/com/example/easypass/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.easypass;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,12 +9,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class HomeFragment extends Fragment {
+    FloatingActionButton addLoginBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View homeView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        addLoginBtn = (FloatingActionButton) homeView.findViewById(R.id.add_login_fab);
+
+        addLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createLoginActivityIntent = new Intent(getActivity(), CreateLoginActivity.class);
+                startActivity(createLoginActivityIntent);
+            }
+        });
+
+        return homeView;
     }
 }
