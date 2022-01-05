6cc3b527758f4d36a7bdc5a6ce9c199896fcd926
ardhan <ardhan.s.pratama@gmail.com>
2022-01-05 18:36:30 +0800

    Added login creation screen

---
+++ app/src/main/java/com/example/easypass/CreateLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.easypass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_login);
+    }
+}
