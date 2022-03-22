fcea79f20ef426edea8049e008e5d5b650fb93b9
ardhan <ardhan.s.pratama@gmail.com>
2022-03-22 21:31:44 +0800

    Updated build.gradle

--- app/src/main/java/com/example/easypass/database/Login.java
+++ app/src/main/java/com/example/easypass/database/Login.java
@@ -3,6 +3,7 @@ package com.example.easypass.database;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
