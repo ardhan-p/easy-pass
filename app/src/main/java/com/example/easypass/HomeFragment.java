ba7907f7cbaf96a276c4eaea834b2bf7a1f59533
ardhan <ardhan.s.pratama@gmail.com>
2022-03-22 18:23:26 +0800

    Added RecyclerView functionality to view logins

--- app/src/main/java/com/example/easypass/HomeFragment.java
+++ app/src/main/java/com/example/easypass/HomeFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
@@ -12,10 +13,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.easypass.database.AppDatabase;
+import com.example.easypass.database.Login;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
     private FloatingActionButton addLoginBtn;
+    private ArrayList<Login> loginsArrayList;
     private RecyclerView loginsRecyclerView;
 
     @Override
@@ -23,8 +28,11 @@ public class HomeFragment extends Fragment {
         View homeView = inflater.inflate(R.layout.fragment_home, container, false);
 
         loginsRecyclerView = homeView.findViewById(R.id.home_password_list);
+        loginsArrayList = new ArrayList<Login>();
 
-
+        LoginObjectListAdapter adapter = new LoginObjectListAdapter(loginsArrayList);
+        loginsRecyclerView.setAdapter(adapter);
+        loginsRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
 
 
         addLoginBtn = (FloatingActionButton) homeView.findViewById(R.id.add_login_fab);
--- app/src/main/java/com/example/easypass/LoginObjectListAdapter.java
+++ app/src/main/java/com/example/easypass/LoginObjectListAdapter.java
@@ -1,5 +1,82 @@
 package com.example.easypass;
 
-public class LoginObjectListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.easypass.database.Login;
+
+import java.util.List;
+
+// https://guides.codepath.com/android/using-the-recyclerview
+public class LoginObjectListAdapter extends RecyclerView.Adapter<LoginObjectListAdapter.ViewHolder> {
+    private List<Login> loginList;
+
+    public LoginObjectListAdapter(List<Login> loginList) {
+        this.loginList = loginList;
+    }
+
+    @NonNull
+    @Override
+    public LoginObjectListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // creates a loginview object and inflates it with the XML file item_login_password
+        View loginView = inflater.inflate(R.layout.item_login_password, parent, false);
+
+        // returns viewholder that contains the newly inflated loginview object
+        ViewHolder vh = new ViewHolder(loginView);
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // sets current viewholder's visual data with the position's login object
+        Login currentLogin = loginList.get(position);
+
+        TextView titleText = holder.loginTitle;
+        titleText.setText(currentLogin.getLoginTitle());
+
+        TextView usernameText = holder.loginUsername;
+        usernameText.setText(currentLogin.getLoginUsername());
+
+        Button copyUsernameBtn = holder.copyUsernameBtn;
+        copyUsernameBtn.setEnabled(true);
+
+        Button copyPasswordBtn = holder.copyPasswordBtn;
+        copyPasswordBtn.setEnabled(true);
+    }
+
+    @Override
+    public int getItemCount() {
+        return loginList.size();
+    }
+
+    // contains the login object, placed in a viewholder
+    // has constructor to create a new row from login object
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView loginTitle;
+        public TextView loginUsername;
+        public TextView loginPassword;
+        public Button copyUsernameBtn;
+        public Button copyPasswordBtn;
+
+        public ViewHolder(View loginView) {
+            super(loginView);
+
+            loginTitle = (TextView) loginView.findViewById(R.id.loginItemTitle);
+            loginUsername = (TextView) loginView.findViewById(R.id.loginItemUsernameView);
+            loginPassword = (TextView) loginView.findViewById(R.id.loginItemPasswordView);
+            copyUsernameBtn = (Button) loginView.findViewById(R.id.copyUsernameButton);
+            copyPasswordBtn = (Button) loginView.findViewById(R.id.copyPasswordButton);
+        }
+    }
 }
